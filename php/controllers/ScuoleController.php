600936c5afa5566b4800954e4a4511f881c942cb
lai2-24 <lai2-24@itismeucci.com>
2025-04-15 13:28:31 +0200

    first commit

--- php/controllers/ScuoleController.php
+++ php/controllers/ScuoleController.php
@@ -2,11 +2,11 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-class DocentiController
+class ScuoleController
 {
   public function index(Request $request, Response $response, $args){
     $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
-    $result = $mysqli_connection->query("SELECT * FROM docenti where scuola_id = " .$args['scuola_id'] );
+    $result = $mysqli_connection->query("SELECT * FROM scuole");
     $results = $result->fetch_all(MYSQLI_ASSOC);
 
     $response->getBody()->write(json_encode($results));
@@ -15,8 +15,8 @@ class DocentiController
 
   public function show(Request $request, Response $response, $args){
     $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
-    $stmt = $mysqli_connection->prepare("SELECT * FROM docenti WHERE scuola_id = ? AND id = ?");
-    $stmt->bind_param("ii", $args['id_scuola'], $args['id_docente']);
+    $stmt = $mysqli_connection->prepare("SELECT * FROM scuole WHERE id = ?");
+    $stmt->bind_param("i", $args['id_scuola']);
     $stmt->execute();
     $result = $stmt->get_result();
     $results = $result->fetch_all(MYSQLI_ASSOC);
@@ -30,8 +30,8 @@ class DocentiController
     
     $data = json_decode($request->getBody()->getContents(), true);
 
-    $stmt = $mysqli_connection->prepare("INSERT INTO docenti (nome, cognome, scuola_id) VALUES (?, ?, ?)");
-    $stmt->bind_param("ssi", $data['nome'], $data['cognome'], $args['id_scuola']);
+    $stmt = $mysqli_connection->prepare("INSERT INTO scuole (nome, indirizzo) VALUES (?, ?)");
+    $stmt->bind_param("ss", $data['nome'], $data['indirizzo']);
     $stmt->execute();
 
     // Chiudi lo statement
@@ -40,7 +40,7 @@ class DocentiController
     //Risposta di successo
     $response->getBody()->write(json_encode([
         'status' => 'success',
-        'message' => 'Docente creato con successo',
+        'message' => 'Scuola creato con successo',
         'id' => $mysqli_connection->insert_id
     ]));
     
@@ -48,14 +48,14 @@ class DocentiController
   }
 
   public function update(Request $request, Response $response, $args) {
-    //curl -X PUT http://localhost:8080/scuole/2/docenti/4 -H "Content-Type: application/json" -d '{"nome": "Ruji", "cognome": "Chen"}'
+    //curl -X PUT http://localhost:8080/scuole/3 -H "Content-Type: application/json" -d '{"nome": "Ruji", "indirizzo": "Ciao"}'
     $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
     // Recupera i dati dal body della richiesta (JSON)
     $data = json_decode($request->getBody()->getContents(), true);
 
     // Prepara la query SQL
-    $stmt = $mysqli_connection->prepare("UPDATE docenti SET nome = ? AND cognome = ? WHERE scuola_id = ? AND id = ?");
-    $stmt->bind_param("ssi", $data['nome'], $data['cognome'], $args['id_scuola'], $args['id_docente']);
+    $stmt = $mysqli_connection->prepare("UPDATE scuole SET nome = ? AND indirizzo = ? WHERE id = ?");
+    $stmt->bind_param("ssi", $data['nome'], $data['indirizzo'], $args['id_scuola']);
     $stmt->execute();
     
     // Chiudi lo statement
@@ -64,7 +64,8 @@ class DocentiController
     //Risposta di successo
     $response->getBody()->write(json_encode([
       'status' => 'success',
-      'message' => 'Docente Aggiornato con successo'
+      'message' => 'Scuola Aggiornato con successo',
+      'id' => $mysqli_connection->insert_id
     ]));
     
     return $response->withHeader("Content-type", "application/json")->withStatus(201); 
@@ -74,8 +75,8 @@ class DocentiController
     $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
     
     // Prepara la query SQL
-    $stmt = $mysqli_connection->prepare("DELETE FROM docenti WHERE scuola_id = ? AND id = ?");
-    $stmt->bind_param("ii", $args['id_scuola'], $args['id_docente']);
+    $stmt = $mysqli_connection->prepare("DELETE FROM scuole WHERE id = ?");
+    $stmt->bind_param("i", $args['id_scuola']);
     $stmt->execute();
     // Chiudi lo statement
     $stmt->close();
@@ -83,7 +84,7 @@ class DocentiController
     //Risposta di successo
     $response->getBody()->write(json_encode([
       'status' => 'success',
-      'message' => 'Docente Eliminato con successo',
+      'message' => 'Scuola Eliminato con successo',
     ]));
     
     return $response->withHeader("Content-type", "application/json")->withStatus(201); 
--- php/index.php
+++ php/index.php
@@ -3,9 +3,32 @@ use Slim\Factory\AppFactory;
 
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/controllers/DocentiController.php';
+require __DIR__ . '/controllers/ScuoleController.php';
 
 $app = AppFactory::create();
+//Scuole
+//curl http://localhost:8080/scuole
+$app->get('/scuole', "ScuoleController:index");
+//curl http://localhost:8080/scuole/1
+$app->get('/scuole/{id_scuola}', "ScuoleController:show");
+//curl -X POST http://localhost:8080/scuole -H "Content-Type: application/json" -d '{"nome": "Scuola Superiore Agnoletti","indirizzo": "via delle corti 100"}'
+$app->post('/scuole', "ScuoleController:create");
+//curl -X PUT http://localhost:8080/scuole/3 -H "Content-Type: application/json" -d '{"nome": "Ruji"}'
+$app->put('/scuole/{id_scuola}', "ScuoleController:update");
+//curl -X DELETE http://localhost:8080/scuole/3
+$app->delete('/scuole/{id_scuola}', "ScuoleController:destroy");
 
+
+//Docenti
+//curl http://localhost:8080/scuole/1/docenti
 $app->get('/scuole/{scuola_id}/docenti', "DocentiController:index");
+//curl http://localhost:8080/scuole/1/docenti/{id_docente}
+$app->get('/scuole/{id_scuola}/docenti/{id_docente}', "DocentiController:show");
+//curl -X POST http://localhost:8080/scuole/2/docenti -H "Content-Type: application/json" -d '{"nome": "nome", "cognome": "cognome"}'
+$app->post('/scuole/{id_scuola}/docenti', "DocentiController:create");
+//curl -X PUT http://localhost:8080/scuole/3/docenti/2 -H "Content-Type: application/json" -d '{"nome": "Ruji", "cognome": "Chen"}'
+$app->put('/scuole/{id_scuola}/docenti/{id_docente}', "DocentiController:update");
+//curl -X DELETE http://localhost:8080/scuole/3/docenti/2
+$app->delete('/scuole/{id_scuola}/docenti/{id_docente}', "DocentiController:destroy");
 
 $app->run();
